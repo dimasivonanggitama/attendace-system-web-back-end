970a1567e7dfc26eb191403f502ee9f3bf0e2dfd
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-14 09:35:50 +0700

    BE-RORO-v7 Add route to fetch method on Role controller

--- src/routes/role.js
+++ src/routes/role.js
@@ -1,6 +1,7 @@
 const { roleController } = require("../controllers");
 const router = require("express").Router();
 
+router.post("/", roleController.fetch);
 router.post("/create", roleController.create);
 
 module.exports = router;
