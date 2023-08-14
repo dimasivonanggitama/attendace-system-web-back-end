ff23f34ec9936307df19b0b34597fd9df8059d86
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-14 09:09:11 +0700

    BE-ROCO-v4 Added codes for save Role to database

--- src/controllers/roleController.js
+++ src/controllers/roleController.js
@@ -4,13 +4,23 @@ const role = db.Role;
 const RoleController = {
     create: async (req, res) => {
         try {
-            return res.status(200).json({
-                message: "You reached Role Controller"
-            })
-        } catch (error) {
+            const { roleName } = req.body;
+            await db.sequelize.transaction(async (t) => {
+                const result = await role.create({
+                    role_name: roleName
+                })
+            }, { transaction: t });
 
+            return res.status(200).json({
+                message: `Role ${roleName} berhasil ditambahkan.`
+            })
+        } catch (err) {
+            return res.status(503).json({
+                message: 'Mohon maaf, layanan tidak tersedia saat ini. Silakan coba lagi nanti.',
+                error: err.message
+            });
         }
-    }
+    },
 }
 
 module.exports = RoleController;
