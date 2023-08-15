7e9fd16f0978f14e4ac6526eabf82a07104635fb
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-15 18:23:35 +0700

    BE-RORO-v9 Integrated edit method on Role controller

--- src/routes/role.js
+++ src/routes/role.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 
 router.get("/", roleController.fetch);
 router.post("/create", roleController.create);
+router.patch("/edit", roleController.edit);
 
 module.exports = router;
