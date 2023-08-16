8f3cae9491f07c4cde3f2db7dd80cd72ca28676b
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-16 10:18:35 +0700

    BE-USCO-v2 Added fetch method

---
+++ src/controllers/userController.js
@@ -0,0 +1,18 @@
+const db = require("../models");
+const user = db.User;
+
+const UserController = {
+    fetch: async (req, res) => {
+        try {
+            const result = await user.findAll();
+            return res.status(200).send(result);
+        } catch (err) {
+            return res.status(503).json({
+                message: 'Mohon maaf, sedang dilakukan pemeliharaan layanan saat ini. Silakan coba lagi nanti.',
+                error: err.message
+            });
+        }
+    },
+}
+
+module.exports = UserController;
