086facd96e8779de67bc150c4b39171ff51a71a1
Dimas Ivon Anggitama <dimasivonanggitama@gmail.com>
2023-08-16 08:40:03 +0700

    BE-RORO-v11 Added route for delete role

--- src/routes/role.js
+++ src/routes/role.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 
 router.get("/", roleController.fetch);
 router.post("/create", roleController.create);
+router.delete("/delete", roleController.delete);
 router.patch("/update", roleController.update);
 
 module.exports = router;
